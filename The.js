5c8b0863b21fa00f0fb4104f7eaa056e803d871e
Tobias Buschor <tobias.buschor@shwups.ch>
2010-10-29 10:23:43 +0200

    diverses

---
+++ The.js
@@ -0,0 +1,326 @@
+// useful: http://kangax.github.com/es5-compat-table/
+
+The = function(){
+  var undf, k, d=document, w=window, slice=[].slice,
+  vendor = w.controllers?'moz':(w.opera?'o':(d.all?'ms':'webkit'))
+  ,Ext = {
+    String:{
+      camelize:function(){ return this.rpl(/-+(.)?/g, function(match, chr){ return chr ? chr.toUpperCase() : '' }) },
+      contains:function(str, separ){ separ=separ||''; return (separ+this+separ).indexOf(separ+str+separ) > -1; },
+      rpl:String.prototype.replace,
+      trim: function(){ return this.rpl(/^\s+|\s+$/g, '');}
+    },
+    Number:{
+      limit: function(min, max){ return Math.min(max, Math.max(min, this)); }
+    },
+    Function:{
+     bind: function(bind){
+       var fn = this;
+       return function(){
+          return fn.apply(bind,arguments);
+       };
+     },
+     rThis: function(){ // if function returns undefined, it now returns "this"
+       var fn = this;
+       return function(){
+         var ret = fn.apply(this,arguments);
+         return ret===undf?this:ret;
+       }
+     },
+     each:function(){ // make a funktion that calls itself for every properties of its instance
+        var fn = this;
+        return function(){
+          var args = arguments, ret = [];
+          slice.call(this).forEach( function(el){
+            var v = fn.apply(el,args);
+            v && ret.push(v);
+          })
+          return ret;
+        }
+      },
+      ignoreUntil: function(min,max){ // waits for the execution of the function (min) and then executes the last call, but waits maximal (max) millisecunds
+        var fn = this, rmMin = $.fn, rmMax = $.fn, hasMax;
+        return function(){
+          var inst=this, args=arguments,
+          wait = function(){
+            hasMax = 0;
+            rmMax();
+            rmMin();
+            fn.apply(inst,args);
+          };
+          rmMin();
+          rmMin = $.wait( wait, min);
+          if(hasMax || !max) return;
+          hasMax = 1
+          rmMax = $.wait( wait, max);
+        }
+      },
+      args: function(){ // make a function with defined Arguments
+        var fn = this, args = arguments;
+        return function(){
+          fn.apply(this,args);
+        }
+      }
+    }
+  }
+/*  function ajax(method, url, success){
+    var r = new XMLHttpRequest();
+    r.onreadystatechange = function(){
+      if(r.readyState==4 && (r.status==200 || r.status==0))
+        success(r.responseText);
+    };
+    r.open(method,url,true);
+    r.setRequestHeader('X-Requested-With','XMLHttpRequest');
+    r.send(null);
+  }
+  $.get = function(url, success){ ajax('GET', url, success); };
+  $.post = function(url, success){ ajax('POST', url, success); };
+  $.getJSON = function(url, success){
+    $.get(url, function(json){ success(JSON.parse(json)) });
+  };
+*/
+  function $(n){ return n.rThis ? n($) : d.getElementById(n) }
+  $.fn = function(v){return v}
+  $.ext = function(target, src){ target=target||{}; for(k in src) !target[k] && (target[k] = src[k]); return target; };
+  $.extEl = function(src){
+    for(k in src){
+      window[k] = Document.prototype[k] = HTMLElement.prototype[k] = src[k].rThis();
+      HTMLCollection.prototype[k] = NodeList.prototype[k] = src[k].rThis().each();
+    }
+  }
+  $.ready= function(fn){ d.readyState==='complete'?fn():$(d).bind('DOMContentLoaded',fn); }
+  $.wait = function(fn,v){ return clearTimeout.args( setTimeout(fn,v) ); }
+  $.use= function(lib,cb,supports,target){
+    var self = $.use;
+    target = target||window;
+    supports = supports||lib;
+    cb=cb||function(){};
+
+    if( target[supports] ){ // loadet
+      return cb( target[supports] );
+    }
+    if( self.cbs[lib] ){ // else loading ?
+      return self.cbs[lib].push(cb);
+    }
+    self.cbs[lib] = [cb]; // else load
+    var tmp = d.createElement('script');
+    tmp.src = self.path+lib+'.js';
+    tmp.on('load', function(cb){
+      while( (cb = self.cbs[lib].shift()) ){
+        cb(target[supports]);
+      }
+    });
+    //tmp.setAttribute('async', 'true');
+    d.documentElement.ft().appendChild(tmp);
+    //d.documentElement.firstChild.appendChild(tmp); wieso geht das beim google-analytics-script??
+  }
+
+  for( k in Ext ){ $.ext(w[k].prototype,Ext[k]); }
+  $.extEl({
+    css: function(prop, value){
+      if(prop.trim) {
+        //        if(value === undf) return this.style[prop.camelize()] || this.style[(vendor+'-'+prop).camelize()];
+        if(value === undf) return (getComputedStyle(this,null).getPropertyValue(prop) || getComputedStyle(this,null).getPropertyValue('-'+vendor+'-'+prop))+'';
+        this.style.cssText += ';'+prop+":"+value+';-'+vendor+'-'+prop+':'+value;
+	    } else for(k in prop) this.css(k,prop[k]);
+    },
+    anim: function(props, dur, ease){
+      //props.transform && (props.transform += ' '+this.css('transform'));
+      props.transition = 'all '+(dur||0.5)+'s '+(ease||'');
+      this.css('transition','all '+(dur||0.5)+'s '+(ease||''))
+      return this.css(props);
+    },
+    adCl: function(v){ !this.hsCl(v) && (this.className += ' '+v)},
+    rmCl: function(v){ this.className = this.className.rpl(new RegExp("(^|\\s)"+v+"(\\s|$)"), '');},
+    hsCl: function(v){ return this.className.contains(v,' '); },
+    els: function(sel){ return this.querySelectorAll(sel); },
+    el: function(sel){ return this.querySelector(sel); },
+    is: function(sel){
+        return (
+          sel.dlg ? sel===this 
+          : (
+              this.matchesSelector
+              ||this[vendor+'MatchesSelector']
+              || function(){ return d.body.els(sel).is(this).length}
+            ).bind(this)(sel)
+        ) && this; 
+    },
+    pv: function(){return this.previousElementSibling},
+    nx: function(){ return this.nextElementSibling },
+    ft: function(){ return this.firstElementChild },
+    lt: function(){ return this.lastElementChild },
+    p: function(){ return this.parentNode },
+    ch: function(sel){ return !sel ? this.children : this.ch().is(sel) },
+    rm: function(){ return this.p().removeChild(this) },
+    on: function(ev,cb){
+      //this.addEventListener(ev, function(e){ var x = {}; $.ext(x,e); cb(x) }, false); // ie9
+      this.addEventListener(ev, cb, false);
+      return this.no.bind(this).args(ev,cb);
+    },
+    no: function(ev,cb){
+      this.removeEventListener(ev, cb, false);
+    },
+    dlg: function(sel, ev, cb){
+      return this.on(ev, function(ev){
+        var el = ev.target;
+        while(el && !el.is(sel)) el = el.p();
+        if(el && !(el===this) && !(el===d)) cb(el, ev);
+      });
+    }
+  });
+  var t = d.els('script');
+  $.use.path = t[t.length-1].src.rpl(/\/[^\/]+$/,'/');
+  $.use.cbs = {};
+  $.Eventer = {
+    initEvent:function(n){
+      !this.Events && (this.Events={});
+      !this.Events[n] && (this.Events[n]=[]);
+    }
+    ,on:function(n,fn){
+        this.initEvent(n);
+        this.Events[n].push(fn);
+    }
+    ,no:function(n,fn){
+        this.initEvent(n);
+        var Events = this.Events[n];
+        Events.splice( Events.indexOf(fn) ,1);
+    }
+    ,fire:function(n,e){
+        this.initEvent(n);
+        var self = this;
+        this.Events[n].forEach(function(fn){
+          fn.bind(self)(e)
+        })
+    }
+  }
+
+  w.$||(w.$=$);
+  return $;
+}();
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Compatibility width jquery
+/*
+jQuery = function(){
+  var slice=[].slice, d=document,w = window,
+    ADJ_OPS={append: 'beforeEnd', prepend: 'afterBegin', before: 'beforeBegin', after: 'afterEnd'},
+    e, k, css, undef;
+
+  function $$(el, selector){ return slice.call(el.querySelectorAll(selector)) }
+  function compact(array){ return array.filter(function(el){ return el !== undef && el !== null }) }
+  
+  function $(_, context){
+    if(context !== undef) return $(context).find(_);
+    function fn(_){ return fn.dom.forEach(_), fn }
+    fn.dom = compact((typeof _ == 'function' && 'dom' in _) ? 
+      _.dom : (_ instanceof Array ? _ : 
+//        (_ instanceof Element ? [_] : 
+        (_.nodeName ? [_] : 
+          $$(d, fn.selector = _))));
+    $.extend(fn, $.fn);
+    return fn;
+  }
+  $.extend = function(target, src){ for(k in src) target[k] = src[k] }
+
+  $.fn = {
+    compact: function(){ this.dom=compact(this.dom); return this },
+    get: function(idx){ return idx === undef ? this.dom : this.dom[idx] },
+    remove: function(){ return this(function(el){ el.rm() });},
+    each: function(callback){ return this(callback) },
+    filter: function(selector){ return $(this.dom.filter(function(el){ return $$(el.p(), selector).indexOf(el)>=0; })); },
+    is: function(selector){ return this.dom[0].is(selector); },
+    first: function(callback){ this.dom=compact([this.dom[0]]); return this },
+    find: function(selector){
+      return $(this.dom.map(function(el){ return el.els(selector) }).reduce(function(a,b){ return a.concat(b) }, []));
+    },
+    closest: function(selector){
+      var el = this.dom[0].parentNode, nodes = $$(d, selector);
+      while(el && nodes.indexOf(el)<0) el = el.parentNode;
+      return $(el && !(el===d) ? el : []);
+    },
+    pluck: function(property){ return this.dom.map(function(el){ return el[property] }) },
+    show: function(){ return this.css('display', 'block') },
+    hide: function(){ return this.css('display', 'none') },
+    prev: function(){ return $(this.pluck('previousElementSibling')) },
+    next: function(){ return $(this.pluck('nextElementSibling')) },
+    html: function(html){
+      return html === undef ? (this.dom.length>0 ? this.dom[0].innerHTML : null) : this(function(el){ el.innerHTML = html });
+    },
+    attr: function(name,value){
+      return (typeof name == 'string' && value === undef) ? (this.dom.length>0 ? this.dom[0].getAttribute(name) || undefined : null) :
+        this(function(el){
+          if (typeof name == 'object') for(k in name) el.setAttribute(k, name[k])
+          else el.setAttribute(name,value);
+        });
+    },
+    offset: function(){
+      var obj = this.dom[0].getBoundingClientRect();
+      return { left: obj.left+d.body.scrollLeft, top: obj.top+d.body.scrollTop, width: obj.width, height: obj.height };
+    },
+    css: function(prop, value){
+      if(value === undef && typeof prop == 'string') return this.dom[0].style[camelize(prop)];
+      css=""; for(k in prop) css += k+':'+prop[k]+';-'+vendor+'-'+k+':'+prop[k];
+      if(typeof prop == 'string') css = prop+":"+value;
+      return this(function(el) { el.style.cssText += ';' + css });
+    },
+    index: function(el){
+      return this.dom.indexOf($(el).get(0));
+    },
+    bind: function(event, callback){
+      return this(function(el){
+        event.split(/\s/).forEach(function(event){ el.addEventListener(event, callback, false); });
+      });
+    },
+    delegate: function(selector, event, callback){
+      return this(function(el){
+        el.on(event, function(event){
+          var target = event.target, nodes = $$(el, selector);
+          while(target && nodes.indexOf(target)<0) target = target.parentNode;
+          if(target && !(target===el) && !(target===d)) callback(target, event);
+        }, false);
+      });
+    },
+    live: function(event, callback){
+      $(d.body).delegate(this.selector, event, callback); return this;
+    },
+    hasClass: function(name){ return this.dom[0].hsCl(name); },
+    addClass: function(name){ return this(function(el){ el.adCl(name) }); },
+    removeClass: function(name){ return this(function(el){ el.rmCl(name) }); },
+    trigger: function(event){
+      return this(function(el){ var e; el.dispatchEvent(e = d.createEvent('Events'), e.initEvent(event, true, false)) });
+    }
+  };
+  
+  ['width','height'].forEach(function(m){ $.fn[m] = function(){ return this.offset()[m] }});
+
+  for(k in ADJ_OPS)
+    $.fn[k] = (function(op){
+      return function(html){ return this(function(el){
+        el['insertAdjacent' + (html instanceof Element ? 'Element' : 'HTML')](op,html)
+      })};
+  })(ADJ_OPS[k]);
+
+  return $;
+}(The);
+
+mootools = function(_){
+	_.extEl({
+	  addEvent:HTMLElement.prototype.on,
+	  removeEvent:HTMLElement.prototype.no,
+	  hasClass:HTMLElement.prototype.hsCl,
+	  addClass:HTMLElement.prototype.adCl,
+	  removeClass:HTMLElement.prototype.rmCl,
+	  getParent:HTMLElement.prototype.p,
+	  getNext:HTMLElement.prototype.nx,
+	  getPrevious:HTMLElement.prototype.pv,
+	  getFirst:HTMLElement.prototype.ft,
+	  last:HTMLElement.prototype.lt,
+	  setStyle:HTMLElement.prototype.css,
+	  getStyle:HTMLElement.prototype.css,
+	  getElement:HTMLElement.prototype.el,
+	  getElements:HTMLElement.prototype.els
+	})
+}(The);
+*/
---
+++ scrollView.js
@@ -0,0 +1,150 @@
+/*
+Basic Use: 
+<div id="scrollable">
+  <div>slide1</div>
+  <div>slide2</div>
+</div>
+<script>
+new scrollableView('scrollable');
+*/
+// $(div).el('.slides').anim({transform:'translateX(-100px)'},2)
+
+
+The(function($){
+
+  /* 
+    scrollView
+  */
+  PointFollower = function(el){
+    var 
+    self=this
+    ,f = {
+      start:{y:0,x:0,t:0},
+      diff:{x:0,y:0,t:0},
+      last:{x:0,y:0,t:0},
+      direction:function(){ return f.diff.y/f.diff.x; },
+      speed:function(){ return Math.sqrt( (f.diff.x*f.diff.x)+(f.diff.y*f.diff.y) ) * 1000 / f.diff.t; },
+      speedX:function(){ return f.diff.x * 1000 / f.diff.t; },
+      speedY:function(){ return f.diff.y * 1000 / f.diff.t; },
+      distance:function(){ return Math.sqrt( Math.pow(f.last.x - f.start.x,2)+Math.pow(f.last.y-f.start.y,2) ); },
+      distanceX:function(){ return f.last.x - f.start.x; },
+      distanceY:function(){ return f.last.y - f.start.y; },
+      duration:function(){ return f.start.t - f.last.t; },
+    }
+    ,s = function(ev){
+  		f.start = f.last = getPoint(ev);
+  		f.diff = {x:0,y:0,t:0};
+    	el.on('touchmove',m);
+    	el.on('touchend',e);
+    	document.on('mousemove',m);
+    	document.on('mouseup',e);
+    	self.fire('start',f);
+    }
+    ,m = function(ev){
+      var point = getPoint(ev);
+      f.diff = {
+        x : point.x - f.last.x,
+        y : point.y - f.last.y, 
+        t : f.last.t - point.t
+      };
+      f.last = point;
+      self.fire('move',f);
+    }
+  	,e = function(ev){
+    	el.no('touchmove',m);
+    	el.no('touchend',e);
+    	document.no('mousemove',m);
+    	document.no('mouseup',e);
+    	self.fire('end',f);
+    },
+    getPoint = function(e){
+      var p = e.targetTouches ? e.targetTouches[0] : e;
+      return {x:p.pageX,y:p.pageY,t:e.timeStamp}; //return {x:e.clientX,y:e.clientY};
+  	}
+  	el.on('touchstart',s);
+  	el.on('mousedown',s);
+  }
+  PointFollower.prototype = $.Eventer;
+
+
+
+
+  /* 
+    scrollView
+  */
+  scrollable = {
+    pos:{}
+    ,init:function(div,opts){
+      var th = this;
+      th.div = $(div).adCl('scrollView')
+    	th.el = th.div.ch('.slides')[0]
+      th.opts = $.ext(opts,{ 
+        speed:1 
+      })
+  	  var p = new PointFollower(th.el);
+    	p.on('move',function(e){
+    		var mx = getMatrix(th.el);
+    		th.pos = {x: mx[4]||0 , y: mx[5]||0};
+    		th.move(e);
+    	});
+    	p.on('end',th.end.bind(th));
+    	th.div.on('touchstart', function(e){e.preventDefault()} );
+    }
+  	,end: function(e){
+    	this.el.css('transition','all 0.6s ease-out');
+  		this.setXY({
+        x:this.pos.x-(e.speedX()/5),
+        y:this.pos.y,
+      })
+  	}
+  	,move: function(e){
+  		if( Math.abs(e.direction()) < 0.6){
+    		this.el.css('transition','none');
+    		this.setXY({
+          x:this.pos.x + e.diff.x*(this.opts.speed),
+          y:this.pos.y + e.diff.y*(this.opts.speed)
+        })
+  		}
+    }
+  	,setXY: function(pos,undf){
+      var 
+      x = pos.x.limit(this.div.offsetWidth-this.el.offsetWidth,0),
+      y = pos.y.limit(this.div.offsetHeight-this.el.offsetHeight,0);
+      this.el.css('transform','translate('+x+'px,'+y+'px)');
+    }
+  }
+  
+  
+  
+  scrollView = function(el,opts){
+    this.init(el,opts);
+  }
+  scrollView.prototype = scrollable;
+  scrollView.prototype.active = 0;
+  scrollView.prototype.xmove = function(e){
+		if( Math.abs(e.direction()) < 0.6){
+  		this.el.css('transition','none');
+  		this.setXY({
+        x:this.pos.x + e.diff.x*(this.opts.speed),
+        y:this.pos.y + e.diff.y*(this.opts.speed)
+      })
+		}
+  }
+  scrollView.prototype.xend = function(e){
+    var add = Math.round( e.speedX()/900 )
+    if( add && Math.abs(e.distanceX()) > 40 ){
+			this.active = (this.active+add).limit(1,this.el.children.length)
+		}
+  	this.el.css('transition','all 0.6s ease-out');
+		this.setXY({
+      x:(this.active-1)*-this.div.offsetWidth,
+      y:this.pos.y
+    })
+  }
+  
+  function getMatrix(el){
+      var i, matrix = el.css('transform').rpl('matrix(','').split(',');
+      for(i in matrix){ matrix[i] = parseFloat( matrix[i].trim() ) }
+      return matrix;
+  }
+})
